573eef01bd936c208a4eb398023dafefb18ea366
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-06-05 10:56:57 -0700

    Created neighborTrees method

--- src/Fire.java
+++ src/Fire.java
@@ -1,17 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fire {
     /**
      * Returns how long it takes for all vulnerable trees to be set on fire if a
      * match is lit at a given location.
      * 
-     * The forest is represented via a rectangular 2d char array where t represents a tree
+     * The forest is represented via a rectangular 2d char array where t represents
+     * a tree
      * and . represents an empty space.
      * 
-     * At time 0, the tree at location [matchR, matchC] is set on fire. At every subsequent
-     * time step, any tree that is adjacent (up/down/left/right) to a burning tree is also
-     * set on fire. 
+     * At time 0, the tree at location [matchR, matchC] is set on fire. At every
+     * subsequent
+     * time step, any tree that is adjacent (up/down/left/right) to a burning tree
+     * is also
+     * set on fire.
      * 
      * 
-     * EXAMPLE 
+     * EXAMPLE
      * forest:
      * 
      * t..tttt.t
@@ -25,12 +31,16 @@ public class Fire {
      * Result: 8
      * 
      * Explanation:
-     * At time 0, the tree at (2, 6) is set on fire. At time 1, its adjacent trees also catch on fire
-     * At time 2, the trees adjacent to those catch as well. At time 8, the last tree to catch is at
-     * (0,6). In this example, there is one tree that never burns, so it is not included in the time calculation.
+     * At time 0, the tree at (2, 6) is set on fire. At time 1, its adjacent trees
+     * also catch on fire
+     * At time 2, the trees adjacent to those catch as well. At time 8, the last
+     * tree to catch is at
+     * (0,6). In this example, there is one tree that never burns, so it is not
+     * included in the time calculation.
      * 
      * 
-     * @param forest a 2d array where t represents a tree and . represents the ground
+     * @param forest a 2d array where t represents a tree and . represents the
+     *               ground
      * @param matchR The row the match is lit at
      * @param matchC The column the match is lit at
      * @return the time at which the final tree to be incinerated starts burning
@@ -39,5 +49,32 @@ public class Fire {
         // HINT: when adding to your BFS queue, you can include more information than
         // just a location. What other information might be useful?
         return -1;
+    }
+
+    public static List<int[]> neighborTrees(char[][] forest, int[] location) {
+        int curR = location[0];
+        int curC = location[1];
+        List<int[]> neighbors = new ArrayList<>();
+
+        int[][] directions = {
+                { -1, 0 },
+                { 1, 0 },
+                { 0, -1 },
+                { 0, 1 }
+        };
+
+        for (int[] move : directions) {
+            int newR = curR + move[0];
+            int newC = curC + move[1];
+
+            if (newR >= 0 && newR < forest.length &&
+                    newC >= 0 && newC < forest[newR].length &&
+                    forest[newR][newC] == 't') {
+                int[] validMove = { newR, newC };
+                neighbors.add(validMove);
+            }
+        }
+
+        return neighbors;
     }
 }
